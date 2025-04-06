1b231b695f4a03da9ca494a463b8ffd2c9b5aea3
Matheus <matheustrilhakoch@hotmail.com>
2025-04-05 23:00:48 -0300

    Adicionando caracteres ao char

--- Labirinto.java
+++ Labirinto.java
@@ -9,21 +9,46 @@ class Labirinto {
         this.labirinto = labirinto;
     }
 
-    public static void criaLabirinto(String filename) {
+    public static char[][] criaLabirinto(String filename) {
+        char[][] novoLabirinto = null;
         try {
             File arquivo = new File(filename);
+            Scanner contador = new Scanner(arquivo);
+            int linhas = 0;
+            int colunas = 0;
+            
+            while (contador.hasNextLine()) {
+                String linha = contador.nextLine();
+                colunas = Math.max(colunas, linha.length());
+                linhas++;
+            }
+            contador.close();
+            
+            novoLabirinto = new char[linhas][colunas];
+            
             Scanner leitura = new Scanner(arquivo);
+            int linha = 0;
             while (leitura.hasNextLine()) {
                 String data = leitura.nextLine();
-                System.out.println(data);
+                for (int coluna = 0; coluna < data.length(); coluna++) {
+                    novoLabirinto[linha][coluna] = data.charAt(coluna);
+                }
+                linha++;
             }
+            leitura.close();
         } catch (FileNotFoundException e){
             System.out.println("Erro na leitura do arquivo: " + filename);
             e.printStackTrace();
         }
+        return novoLabirinto;
     }
 
     public static void main(String[] args) {
-        criaLabirinto("labirinto.txt");
+        char[][] lab = criaLabirinto("labirinto.txt");
+        Labirinto novoLabirinto = new Labirinto(lab);
+        
+        for (char[] linha : novoLabirinto.labirinto) {
+            System.out.println(new String(linha));
+        }
     }
 }
