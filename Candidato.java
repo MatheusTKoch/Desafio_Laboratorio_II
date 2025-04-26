6c0dfb2c95f51e4d3f392e263b09c9320a12941f
Matheus <matheustrilhakoch@hotmail.com>
2025-04-26 20:19:35 -0300

    Adicionada classe Candidato

---
+++ Candidato.java
@@ -0,0 +1,41 @@
+public class Candidato {
+    private String nome;
+    private String partido;
+    private int intencoesVotos;
+
+    public Candidato(String nome, String partido, int intencoesVotos) {
+        this.nome = nome;
+        this.partido = partido;
+        this.intencoesVotos = intencoesVotos;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getPartido() {
+        return partido;
+    }
+
+    public void setPartido(String partido) {
+        this.partido = partido;
+    }
+
+    public int getIntencoes() {
+        return intencoesVotos;
+    }
+
+    public void setIntencoes(int intencoesVotos) {
+        this.intencoesVotos = intencoesVotos;
+    }
+
+    @Override
+    public String toString() {
+        return "Dados do candidato - Nome: " + getNome() + ", Partido: " + getPartido() + ", intencoes de voto: " + getIntencoes();
+    }
+    
+}
