e8e4d8a900e9f7c70190f200e4e7bcaf4bb124c0
MatheusTKoch <matheust_koch@hotmail.com>
2025-05-10 17:01:47 -0300

    Adicionada classe para etapa3

--- Etapa3.java
+++ Etapa3.java
@@ -1,3 +1,33 @@
+import java.util.Stack;
+
 public class Etapa3 {
-    
+    public boolean checkBrackets(Stack<Character> s1) {
+        Stack<Character> auxStack = new Stack<>();
+        Stack<Character> tempStack = new Stack<>();
+        
+        int openCount = 0;
+        
+        while (!s1.isEmpty()) {
+            char c = s1.pop();
+            tempStack.push(c);
+        }
+        
+        while (!tempStack.isEmpty()) {
+            char c = tempStack.pop();
+            s1.push(c);
+            
+            if (c == '(') {
+                openCount++;
+                auxStack.push(c);
+            } 
+            else if (c == ')') {
+                openCount--;
+                if (openCount < 0) {
+                    return false;
+                }
+            }
+        }
+        
+        return openCount == 0;
+    }
 }
